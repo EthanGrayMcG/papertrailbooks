37260495d55df6b512575181dbb26cf9a5a7b961
Ethan McGuinness <ethangraymcguinness@gmail.com>
2022-12-23 12:57:09 -0700

    added new source files, archived old site

--- js/scripts.js
+++ js/scripts.js
@@ -1,98 +1,20 @@
 /* Description: Custom JS file */
 
-/* Navigation */
-// Collapse the navbar by adding the top-nav-collapse class
+
+/* For Back To Top Button */
 window.onscroll = function () {
-	scrollFunction();
 	scrollFunctionBTT(); // back to top button
 };
 
-window.onload = function () {
-	scrollFunction();
-};
 
-function scrollFunction() {
-	if (document.documentElement.scrollTop > 30) {
-		document.getElementById("navbarExample").classList.add("top-nav-collapse");
-	} else if ( document.documentElement.scrollTop < 30 ) {
-		document.getElementById("navbarExample").classList.remove("top-nav-collapse");
-	}
-}
-
-// Navbar on mobile
-let elements = document.querySelectorAll(".navbar-nav .nav-link:not(.dropdown-toggle)");
-
-for (let i = 0; i < elements.length; i++) {
-	elements[i].addEventListener("click", () => {
-		document.querySelector(".offcanvas-collapse").classList.toggle("open");
-	});
-}
-
-document.querySelector(".navbar-toggler").addEventListener("click", () => {
-  	document.querySelector(".offcanvas-collapse").classList.toggle("open");
-});
-
-// Hover on desktop
-function toggleDropdown(e) {
-	const _d = e.target.closest(".dropdown");
-	let _m = document.querySelector(".dropdown-menu", _d);
-
-	setTimeout(
-		function () {
-		const shouldOpen = _d.matches(":hover");
-		_m.classList.toggle("show", shouldOpen);
-		_d.classList.toggle("show", shouldOpen);
-
-		_d.setAttribute("aria-expanded", shouldOpen);
-		},
-		e.type === "mouseleave" ? 300 : 0
-	);
-}
-
-// On hover
-const dropdownCheck = document.querySelector('.dropdown');
-
-if (dropdownCheck !== null) { 
-	document.querySelector(".dropdown").addEventListener("mouseleave", toggleDropdown);
-	document.querySelector(".dropdown").addEventListener("mouseover", toggleDropdown);
-
-	// On click
-	document.querySelector(".dropdown").addEventListener("click", (e) => {
-		const _d = e.target.closest(".dropdown");
-		let _m = document.querySelector(".dropdown-menu", _d);
-		if (_d.classList.contains("show")) {
-			_m.classList.remove("show");
-			_d.classList.remove("show");
-		} else {
-			_m.classList.add("show");
-			_d.classList.add("show");
-		}
-	});
-}
-
-
-/* Card Slider - Swiper */
-var cardSlider = new Swiper('.card-slider', {
+/* Image Slider 1 - Swiper */
+var imageSlider = new Swiper('.image-slider-1', {
 	autoplay: {
 		delay: 4000,
 		disableOnInteraction: false
 	},
 	loop: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev'
-	}
-});
-
-
-/* Image Slider - Swiper */
-var imageSlider = new Swiper('.image-slider', {
-	autoplay: {
-		delay: 2000,
-		disableOnInteraction: false
-	},
-	loop: true,
-	spaceBetween: 30,
+	spaceBetween: 60,
 	slidesPerView: 5,
 	breakpoints: {
 		// when window is <= 575px
@@ -103,19 +25,32 @@ var imageSlider = new Swiper('.image-slider', {
 		// when window is <= 767px
 		767: {
 			slidesPerView: 2,
-			spaceBetween: 20
+			spaceBetween: 40
 		},
 		// when window is <= 991px
 		991: {
 			slidesPerView: 3,
-			spaceBetween: 20
+			spaceBetween: 50
 		},
 		// when window is <= 1199px
 		1199: {
 			slidesPerView: 4,
-			spaceBetween: 20
+			spaceBetween: 60
 		},
+	}
+});
 
+
+/* Image Slider 2 - Swiper */
+var imageSlider2 = new Swiper('.image-slider-2', {
+	autoplay: {
+		delay: 3000,
+		disableOnInteraction: false
+	},
+	loop: true,
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
 	}
 });
 
@@ -151,7 +86,7 @@ myButton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 function scrollFunctionBTT() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
 		myButton.style.display = "block";
 	} else {
 		myButton.style.display = "none";
